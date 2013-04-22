17ace47a3805bb6660effc26dfd91b72a41281b3
root <root@localhost.localdomain>
2013-04-23 01:03:37 +0700

    adding y-axis servo

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,18 +1,62 @@
 #include <Servo.h>
 Servo Sx;
-int i = 0;
+Servo Sy;
+int i = 0, p = 0;
 
 void setup() {
   Sx.attach(9);
+  Sy.attach(10);
 }
 
 void loop() {
-  for(i = 0; i <= 70; i += 1){
-    Sx.write(i);
-    delay(200);
-  }
-  for(i = 70; i >= 0; i -= 1){
-    Sx.write(i);
-    delay(200);
+  for(p = 0; p <= 60; p +=5) {
+    for(i = 0; i <= 70; p += 1) {
+      if(i == 70) {
+        Sy.write(p);
+        delay(1000);
+      }
+      else {
+        Sx.write(i);
+        delay(200);
+      }
+    }
+    for(i = 70; i >= 0; i -= 1) {
+      if(i == 0) {
+        Sy.write(p);
+        delay(1000);
+      }
+      else {
+        Sx.write(i);
+        delay(200);
+      }
+    }
   }
 }
+
+/*
+void loop() {
+  for(i = 0; i <= 70; i += 1) {
+    if(i == 70) {
+      for(p = 0; p <= 60; p += 5) {
+      Sy.write(p);
+      delay(200);
+      }
+    }
+    else {
+      Sx.write(i);
+      delay(200);
+    }
+  }
+  for(i = 70; i >= 0; i -= 1) {
+    if(i == 0) {
+      for(p = 0; p <= 60; p += 5) {
+      Sy.write(p);
+      delay(200);
+      }
+    }
+    else {
+      Sx.write(i);
+      delay(200);
+    }
+  }
+}*/
