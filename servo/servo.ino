844e6e84b8d60046b8c0908fe9745a39c3df07c2
h3rucutu <herupranoto.ugm@gmail.com>
2013-04-22 13:24:48 +0700

    5 commit

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,22 +1,23 @@
 #include <Servo.h>
 Servo Sx;
-int i;
+int i = 0;
 
 void setup()
 {
   Sx.attach(9);
 }
 
-void loop()
-{
-  for(i=55; i<=125; i+=1)
-  {
+void loop() {
+  for(i = 0; i <= 70; i += 1){
     Sx.write(i);
     delay(200);
   }
-  for(i=125; i>=55; i-=2)
-  {
+  for(i = 70; i >= 0; i -= 1){
     Sx.write(i);
     delay(200);
+  }
+  if(i == 70 || i == 0) {
+    Sx.write(i);
+    delay(15);
   }
 }
