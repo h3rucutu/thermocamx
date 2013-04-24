da4edfd2d399dd997c3d6161bc005cd65b81eca0
root <root@localhost.localdomain>
2013-04-24 19:22:39 +0700

    servo x and y solved

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,62 +1,38 @@
 #include <Servo.h>
 Servo Sx;
 Servo Sy;
-int i = 0, p = 0;
+int i = 0, j = 5;
 
 void setup() {
   Sx.attach(9);
   Sy.attach(10);
 }
 
-/*void loop() {
-  for(p = 0; p <= 60; p +=5) {
-    for(i = 0; i <= 70; p += 1) {
+void loop() {
+  Sy.write(0);
+  delay(100);
+  while(j <= 60) {
+    for(i = 0; i <= 70; i += 1) {
       if(i == 70) {
-        Sy.write(p);
-        delay(1000);
+        Sy.write(j);
+        delay(100);
       }
       else {
         Sx.write(i);
         delay(200);
       }
     }
+    j += 5;
     for(i = 70; i >= 0; i -= 1) {
       if(i == 0) {
-        Sy.write(p);
-        delay(1000);
+        Sy.write(j);
+        delay(100);
       }
       else {
         Sx.write(i);
         delay(200);
       }
     }
-  }
-}
-*/
-
-void loop() {
-  for(i = 0; i <= 70; i += 1) {
-    if(i == 70) {
-      for(p = 0; p <= 60; p += 5) {
-      Sy.write(p);
-      delay(200);
-      }
-    }
-    else {
-      Sx.write(i);
-      delay(200);
-    }
-  }
-  for(i = 70; i >= 0; i -= 1) {
-    if(i == 0) {
-      for(p = 0; p <= 60; p += 5) {
-      Sy.write(p);
-      delay(200);
-      }
-    }
-    else {
-      Sx.write(i);
-      delay(200);
-    }
+    j += 5;
   }
 }
